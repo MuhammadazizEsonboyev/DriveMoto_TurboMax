a8a42e44e708b0f893592425afdbd7b172bc076e
risqiboyevbobur <brisqiboyev@gmail.com>
2023-03-29 16:34:07 +0500

    Fixed Routers and added mainNav

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import AppThree from "./About/AppThree";
+import AppTwo from "./Category/AppTwo";
 import AppOne from "./Home/AppOne";
-import "./index.css"
+import MainNav from "./Home/MainNav";
+import "./index.css";
 
 function App() {
   return (
-   <>
-      <AppOne/>
-      <AppThree/>
-   </>
-  );
+    <>
    
+      <Router>
+        <MainNav/>
+        <Routes>
+          <Route path="/home" element={<AppOne />} />
+          <Route path="/category" element={<AppTwo />} />
+          <Route path="/about" element={<AppThree />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
