1db66f320f5d3a57c46ac4884f4b3ebd62b9a28a
MuhammadazizEsonboyev <esonboyev03@bk.ru>
2023-03-30 22:33:57 +0500

    fix

--- src/Home/Section/Cards/CardNames.js
+++ src/Home/Section/Cards/CardNames.js
@@ -1,13 +1,20 @@
 const cards4 = require('./image/cards4.png')
 
 const CardData = [
-    {
-        id:1,
-        title: "Водонепроницаемый Рюкзак",
-        price: "9 800 ₽",
-        img: {cards4},
-        amount: 1,
-    },
+  {
+    id: 1,
+    title: 'Водонепроницаемый Рюкзак',
+    price: '9 800 ₽',
+    img: { cards4 },
+    amount: 1
+  },
+  {
+    id: 1,
+    title: 'Водонепроницаемый Рюкзак',
+    price: '9 800 ₽',
+    img: { cards4 },
+    amount: 1
+  }
 ]
 
-export default CardData;
+export default CardData
--- src/Home/Section/Section1.jsx
+++ src/Home/Section/Section1.jsx
@@ -3,41 +3,38 @@ import { Col, Container, Row } from 'react-bootstrap'
 import Cards from './Cards/Cards'
 import CardData from './Cards/CardNames'
 
-const Section1 = ({ handleClick }) => {
+const Section1 = ({ item, handleClick }) => {
   return (
     <>
-
       <Container>
         <Row>
           <Col xs={8}>
-            <h2 className="textFooter">Популярные товары</h2>
-            <ul className="list_footer">
-              <li className="list_item_footer">запчасти</li>
-              <li className="list_item_footer">моторы</li>
-              <li className="list_item_footer">шины </li>
-              <li className="list_item_footer">электроника</li>
-              <li className="list_item_footer">инструменты</li>
-              <li className="list_item_footer">аксессуары</li>
+            <h2 className='textFooter'>Популярные товары</h2>
+            <ul className='list_footer'>
+              <li className='list_item_footer'>запчасти</li>
+              <li className='list_item_footer'>моторы</li>
+              <li className='list_item_footer'>шины </li>
+              <li className='list_item_footer'>электроника</li>
+              <li className='list_item_footer'>инструменты</li>
+              <li className='list_item_footer'>аксессуары</li>
             </ul>
           </Col>
         </Row>
       </Container>
 
-
-      {CardData?.map((item) => {
-        console.log(item);
+      {CardData?.map(item => {
+        // console.log(item);
         return (
           <>
             <Container>
               <Row className='justify-content-center'>
                 <Col xs={3}>
-                  <Cards item={item} key={item.id} handleClick={handleClick} />
+                  <Cards item={{item}} key={item.id} handleClick={handleClick} />
                 </Col>
               </Row>
             </Container>
           </>
         )
-
       })}
     </>
   )
