0d310a631a5cbba6f23b13461fac113de1a8f66c
Behruzaxi <ebehruz846@gmail.com>
2023-03-31 17:45:28 +0500

    fdsa

--- src/Category/AppTwo.js
+++ src/Category/AppTwo.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Bfooter from './Bfooter/Bfooter'
 import Bnavbar from './BNavbar/Bnavbar'
+import Bfooter from '../About/Bfooter/Bfooter'
 
 export default function AppTwo() {
   return (
     <div>
       <Bnavbar/>
       <Bfooter/>
-      
+      <Bfooter/>
     </div>
   )
 }
