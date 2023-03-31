cf346b9ad807953da34d97f37a9731bba66f38f0
AMDINSIDE <umirzakovasad@gmail.com>
2023-03-31 15:00:18 +0500

    get al changes & language

---
+++ src/i18n.js
@@ -0,0 +1,42 @@
+import i18n from 'i18next';
+import Backend from 'i18next-xhr-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import { initReactI18next } from 'react-i18next';
+
+
+const options = {
+  order: [ 'navigator', 'htmlTag', 'path', 'subdomain'],
+
+  lookupQuerystring: 'lng',
+  lookupCookie: 'i18next',
+  lookupLocalStorage: 'i18nextLng',
+  lookupFromPathIndex: 0,
+  lookupFromSubdomainIndex: 0,
+
+  caches: ['localStorage', 'cookie'],
+  excludeCacheFor: ['cimode'], 
+  cookieMinutes: 10,
+  cookieDomain: 'myDomain',
+  htmlTag: document.documentElement,
+  checkWhitelist: true
+}
+
+i18n
+  .use(Backend) // load translation using xhr -> see /public/locales. We will add locales in the next step
+
+  .use(LanguageDetector) // detect user language
+
+  .use(initReactI18next) // pass the i18n instance to react-i18next.
+
+  .init({
+    // fallbackLng, // if user computer language is not on the list of available languages, than we will be using the fallback language specified earlier
+    debug: true,
+    // whitelist: availableLanguages,
+    detection: options,
+
+    interpolation: {
+      escapeValue: false
+    },
+  });
+
+export default i18n;
