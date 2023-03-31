91a2d4b82931dd670473e986bf3fbcdf154b0a38
risqiboyevbobur <brisqiboyev@gmail.com>
2023-03-31 13:18:52 +0500

    fweW

--- src/Category/BNavbar/Bnavbar.jsx
+++ src/Category/BNavbar/Bnavbar.jsx
@@ -23,6 +23,7 @@ const names = ["Our product", "About it", "Prices"];
 
 function getStyles(name, personName, theme) {
   return {
+    
     fontWeight:
       personName.indexOf(name) === -1
         ? theme.typography.fontWeightRegular
