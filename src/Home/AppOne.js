82d8275830a1874045b9cf678b64b722de4e132e
Behruzaxi <ebehruz846@gmail.com>
2023-03-31 17:43:13 +0500

    das

--- src/Home/AppOne.js
+++ src/Home/AppOne.js
@@ -5,7 +5,6 @@ import FooterPage from './Footer/Footer'
 import Section1 from './Section/Section1'
 import Section2 from './Section/Section2'
 import Cart from './Section/Cards/Cart'
-import Bfooter from '../About/Bfooter/Bfooter'
 
 export default function AppOne() {
   const [cart, setCart] = useState([])
@@ -31,7 +30,6 @@ setCart([...arr])
     <>
     <NavbarPage />
     <Header/>
-    <Bfooter/>
     <Section1 handleClick = {handleClick}/>
     <Section2 handleClick = {handleClick}/>
     <FooterPage/>
